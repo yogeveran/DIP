aef6fc6586d6105130a53e0e271d3004c2d9e24c
eran yogev <yogeveran@gmail.com>
2015-03-19 17:53:52 +0200

    Flag Download. Added first attempt of distortion.

--- ImageToFlag/ImageToFlag/ImageToFlag.cpp
+++ ImageToFlag/ImageToFlag/ImageToFlag.cpp
@@ -4,9 +4,12 @@
 #include "stdafx.h"
 #include "core.hpp"
 #include "highgui.hpp"
+#include "imgproc.hpp"
+#include "opencv.hpp"
 #include <iostream>
 using namespace cv;
 using namespace std;
+
 
 int main(int argc, char** argv)
 {
@@ -27,12 +30,27 @@ int main(int argc, char** argv)
 
 	/// Set the dst image the same type and size as src
 	warp_dst = Mat::zeros(src.rows, src.cols, src.type());
-	warp_dst.setTo(cv::Scalar(255, 255, 255));
+	warp_dst.setTo(Scalar(255, 255, 255));
+
+
+	//Distort done here
+	Mat t(2, 3, CV_64F, Scalar(0.0));
+
+	t.at<double>(0, 0) = 1;
+	t.at<double>(1, 1) = 1;
+	t.at<double>(0, 1) = -tan(0.17);
+	//    t.at<double>(2,2) = 1;
+
+
+	Mat dest;
+	Size size(src.cols, src.rows);
+	warpAffine(src, dest, t, size, INTER_LINEAR, BORDER_CONSTANT);
+	//end distort here
 
 	namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
 	namedWindow("Display window2", WINDOW_AUTOSIZE);// Create a window for display.
 	imshow("Display window", src);                   // Show our image inside it.
-	imshow("Display window2", warp_dst);                   // Show our image inside it.
+	imshow("Display window2", dest);                   // Show our image inside it.
 
 	waitKey(0);                                          // Wait for a keystroke in the window
 	return 0;
