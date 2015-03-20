735bfa54b682a990a202d4f4c5bc67f583e25eca
eran yogev <yogeveran@gmail.com>
2015-03-20 19:32:53 +0200

    Fixed Interpolation

--- ImageToFlag/ImageToFlag/ImageToFlag.cpp
+++ ImageToFlag/ImageToFlag/ImageToFlag.cpp
@@ -66,28 +66,28 @@ int main(int argc, char** argv)
 			int src_y;
 			switch (interpolation[0]){
 			case 'n':
-				src_y = ((int)round(y)) % src.rows;
+				src_y = ((int)round(y));
 				break;
 			case 'b'://Bi-Linear ------ Need To Do
 			case 'c'://Cubic	 ------ Need To Do
 			default:
-				src_y = ((int)round(y)) % src.rows;
+				src_y = ((int)round(y));
 				break;
 				
 			}
 			
-			if (src_y >= 0)
+			if ((src_y >= 0) && (src_y<src.rows))
 				warp_dst_inter.at<Vec3b>(new_y, x) = src.at<Vec3b>(src_y, x);
 		}
 	}
-	cout << "done backward  \n";
+	cout << "\ndone backward \n";
 
 	namedWindow("Before", WINDOW_AUTOSIZE);  // Create a window for display.
-	namedWindow("After", WINDOW_AUTOSIZE);   // Create a window for display.
-	namedWindow("After Inter", WINDOW_AUTOSIZE);   // Create a window for display.
+	namedWindow("No Interpolation", WINDOW_AUTOSIZE);   // Create a window for display.
+	namedWindow("With Interpolation", WINDOW_AUTOSIZE);   // Create a window for display.
 	imshow("Before", src);                   // Show Before Image
-	imshow("After", warp_dst);               // Show After  Image
-	imshow("After Inter", warp_dst_inter);   // Create a window for display.
+	imshow("No Interpolation", warp_dst);               // Show After  Image
+	imshow("With Interpolation", warp_dst_inter);   // Create a window for display.
 
 	waitKey(0);                              // Wait for a keystroke in the window
 	return 0;
