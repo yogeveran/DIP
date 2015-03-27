e295c090288c0a1155970e09c4544eb5c180e97f
Javier Dorfsman <javitolin@gmail.com>
2015-03-27 20:06:37 +0300

    Getting closer...

--- ImageToFlag/ImageToFlag/main.cpp
+++ ImageToFlag/ImageToFlag/main.cpp
@@ -12,171 +12,75 @@
 using namespace cv;
 using namespace std;
 Mat leftUp, leftDown, rightUp, rightDown;
-Mat src, dst, dst_inter, srcWithMirror;
+Mat dst, dst_inter, srcWithMirror;
 
 bool in_Range(double value, int from, int to) {
 	return (from <= floor(value)) && (ceil(value) < to);
 }
-long uFunc(long d) {
+float uFunc(float d) {
 	if (d < 1 && d >= 0) {
-		return (1.5 * pow(d, 3) - 2.5 * pow(d, 2) + 1);
+		return abs((1.5 * pow(d, 3) - 2.5 * pow(d, 2) + 1));
 	} else if (d < 2 && d >= 1) {
-		return (-1.5 * pow(d, 3) + 2.5 * pow(d, 2) - 4 * d + 2);
-	} else
+		return abs((-1.5 * pow(d, 3) + 2.5 * pow(d, 2) - 4 * d + 2));
+	} else {
 		return 0;
+	}
 }
 Vec3f uFunc(Vec3f d) {
-	Vec3f ans = Vec3f(uFunc(d(0)),uFunc(d(1)),uFunc(d(2)));
+	Vec3f ans = Vec3f(uFunc(d(0)), uFunc(d(1)), uFunc(d(2)));
 	return ans;
 }
-/*
-void calcLeftUp() {
-	Mat leftUpX = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	Mat leftUpY = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	leftUpX.at<long>(0, 0) = 7 / 4;
-	leftUpY.at<long>(0, 0) = 7 / 4;
-	leftUpX.at<long>(0, 1) = 3 / 4;
-	leftUpY.at<long>(0, 1) = 7 / 4;
-	leftUpX.at<long>(0, 2) = 1 / 4;
-	leftUpY.at<long>(0, 2) = 7 / 4;
-	leftUpX.at<long>(0, 3) = 5 / 4;
-	leftUpY.at<long>(0, 3) = 7 / 4;
-	leftUpX.at<long>(1, 0) = 7 / 4;
-	leftUpY.at<long>(1, 0) = 7 / 4;
-	leftUpX.at<long>(1, 1) = 3 / 4;
-	leftUpY.at<long>(1, 1) = 3 / 4;
-	leftUpX.at<long>(1, 2) = 1 / 4;
-	leftUpY.at<long>(1, 2) = 3 / 4;
-	leftUpX.at<long>(1, 3) = 5 / 4;
-	leftUpY.at<long>(1, 3) = 3 / 4;
-	leftUpX.at<long>(2, 0) = 7 / 4;
-	leftUpY.at<long>(2, 0) = 1 / 4;
-	leftUpX.at<long>(2, 1) = 3 / 4;
-	leftUpY.at<long>(2, 1) = 1 / 4;
-	leftUpX.at<long>(2, 2) = 1 / 4;
-	leftUpY.at<long>(2, 2) = 1 / 4;
-	leftUpX.at<long>(2, 3) = 5 / 4;
-	leftUpY.at<long>(2, 3) = 1 / 4;
-	leftUpX.at<long>(3, 0) = 7 / 4;
-	leftUpY.at<long>(3, 0) = 5 / 4;
-	leftUpX.at<long>(3, 1) = 3 / 4;
-	leftUpY.at<long>(3, 1) = 5 / 4;
-	leftUpX.at<long>(3, 2) = 1 / 4;
-	leftUpY.at<long>(3, 2) = 5 / 4;
-	leftUpX.at<long>(3, 3) = 5 / 4;
-	leftUpY.at<long>(3, 3) = 5 / 4;
-	long sumForNormal = 0;
-	for (int i = 0; i < MAT_SIZE; i++) {
-		for (int j = 0; j < MAT_SIZE; j++) {
-			long uFuncAns = uFunc(leftUpX.at<long>(i, j)) * (uFunc(leftUpY.at<long>(i, j)));
-			leftUp.at<Vec3b>(i, j)(0) = uFuncAns;
-			leftUp.at<Vec3b>(i, j)(1) = uFuncAns;
-			leftUp.at<Vec3b>(i, j)(2) = uFuncAns;
-			sumForNormal += uFuncAns;
-		}
-	}
-	for (int i = 0; i < MAT_SIZE; i++) {
-		for (int j = 0; j < MAT_SIZE; j++) {
-			leftUp.at<Vec3b>(i, j)(0) = leftUp.at<Vec3b>(i, j)(0) / sumForNormal;
-			leftUp.at<Vec3b>(i, j)(1) = leftUp.at<Vec3b>(i, j)(1) / sumForNormal;
-			leftUp.at<Vec3b>(i, j)(2) = leftUp.at<Vec3b>(i, j)(2) / sumForNormal;
-		}
-	}
-}
-void calcLeftDown() {
-	Mat leftDownX = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	Mat leftDownY = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	leftDownX.at<long>(0, 0) = 7 / 4;
-	leftDownY.at<long>(0, 0) = 5 / 4;
-	leftDownX.at<long>(0, 1) = 3 / 4;
-	leftDownY.at<long>(0, 1) = 5 / 4;
-	leftDownX.at<long>(0, 2) = 1 / 4;
-	leftDownY.at<long>(0, 2) = 5 / 4;
-	leftDownX.at<long>(0, 3) = 5 / 4;
-	leftDownY.at<long>(0, 3) = 5 / 4;
-	leftDownX.at<long>(1, 0) = 7 / 4;
-	leftDownY.at<long>(1, 0) = 1 / 4;
-	leftDownX.at<long>(1, 1) = 3 / 4;
-	leftDownY.at<long>(1, 1) = 1 / 4;
-	leftDownX.at<long>(1, 2) = 1 / 4;
-	leftDownY.at<long>(1, 2) = 1 / 4;
-	leftDownX.at<long>(1, 3) = 5 / 4;
-	leftDownY.at<long>(1, 3) = 1 / 4;
-	leftDownX.at<long>(2, 0) = 7 / 4;
-	leftDownY.at<long>(2, 0) = 3 / 4;
-	leftDownX.at<long>(2, 1) = 3 / 4;
-	leftDownY.at<long>(2, 1) = 3 / 4;
-	leftDownX.at<long>(2, 2) = 1 / 4;
-	leftDownY.at<long>(2, 2) = 3 / 4;
-	leftDownX.at<long>(2, 3) = 5 / 4;
-	leftDownY.at<long>(2, 3) = 3 / 4;
-	leftDownX.at<long>(3, 0) = 7 / 4;
-	leftDownY.at<long>(3, 0) = 7 / 4;
-	leftDownX.at<long>(3, 1) = 3 / 4;
-	leftDownY.at<long>(3, 1) = 7 / 4;
-	leftDownX.at<long>(3, 2) = 1 / 4;
-	leftDownY.at<long>(3, 2) = 7 / 4;
-	leftDownX.at<long>(3, 3) = 5 / 4;
-	leftDownY.at<long>(3, 3) = 7 / 4;
-	long sumForNormal = 0;
-	for (int i = 0; i < MAT_SIZE; i++) {
-		for (int j = 0; j < MAT_SIZE; j++) {
-			long uFuncAns = uFunc(leftDownX.at<long>(i, j)) * (uFunc(leftDownY.at<long>(i, j)));
-			leftDown.at<Vec3b>(i, j)(0) = uFuncAns;
-			leftDown.at<Vec3b>(i, j)(1) = uFuncAns;
-			leftDown.at<Vec3b>(i, j)(2) = uFuncAns;
-			sumForNormal += uFuncAns;
-		}
-	}
-	for (int i = 0; i < MAT_SIZE; i++) {
-		for (int j = 0; j < MAT_SIZE; j++) {
-			leftDown.at<Vec3b>(i, j)(0) = leftDown.at<Vec3b>(i, j)(0) / sumForNormal;
-			leftDown.at<Vec3b>(i, j)(1) = leftDown.at<Vec3b>(i, j)(1) / sumForNormal;
-			leftDown.at<Vec3b>(i, j)(2) = leftDown.at<Vec3b>(i, j)(2) / sumForNormal;
-		}
-	}
-}
-*/
 
-void calcRightUp() {
-	Mat rightUpX = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	Mat rightUpY = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	rightUpX.at<long>(0, 0) = 5 / 4;
-	rightUpY.at<long>(0, 0) = 7 / 4;
-	rightUpX.at<long>(0, 1) = 1 / 4;
-	rightUpY.at<long>(0, 1) = 7 / 4;
-	rightUpX.at<long>(0, 2) = 3 / 4;
-	rightUpY.at<long>(0, 2) = 7 / 4;
-	rightUpX.at<long>(0, 3) = 7 / 4;
-	rightUpY.at<long>(0, 3) = 7 / 4;
-	rightUpX.at<long>(1, 0) = 5 / 4;
-	rightUpY.at<long>(1, 0) = 3 / 4;
-	rightUpX.at<long>(1, 1) = 1 / 4;
-	rightUpY.at<long>(1, 1) = 3 / 4;
-	rightUpX.at<long>(1, 2) = 3 / 4;
-	rightUpY.at<long>(1, 2) = 3 / 4;
-	rightUpX.at<long>(1, 3) = 7 / 4;
-	rightUpY.at<long>(1, 3) = 3 / 4;
-	rightUpX.at<long>(2, 0) = 5 / 4;
-	rightUpY.at<long>(2, 0) = 1 / 4;
-	rightUpX.at<long>(2, 1) = 1 / 4;
-	rightUpY.at<long>(2, 1) = 1 / 4;
-	rightUpX.at<long>(2, 2) = 3 / 4;
-	rightUpY.at<long>(2, 2) = 1 / 4;
-	rightUpX.at<long>(2, 3) = 7 / 4;
-	rightUpY.at<long>(2, 3) = 1 / 4;
-	rightUpX.at<long>(3, 0) = 5 / 4;
-	rightUpY.at<long>(3, 0) = 5 / 4;
-	rightUpX.at<long>(3, 1) = 1 / 4;
-	rightUpY.at<long>(3, 1) = 5 / 4;
-	rightUpX.at<long>(3, 2) = 3 / 4;
-	rightUpY.at<long>(3, 2) = 5 / 4;
-	rightUpX.at<long>(3, 3) = 7 / 4;
-	rightUpY.at<long>(3, 3) = 5 / 4;
-	long sumForNormal = 0;
+void calcRightUp(Mat src) {
+	float** rightUpXFloat = new float*[MAT_SIZE];
+	for (int i = 0; i < MAT_SIZE; ++i)
+		rightUpXFloat[i] = new float[MAT_SIZE];
+	float** rightUpYFloat = new float*[MAT_SIZE];
+	for (int i = 0; i < MAT_SIZE; ++i)
+		rightUpYFloat[i] = new float[MAT_SIZE];
+
+	rightUpXFloat[0][0] = 5 / 4;
+	rightUpXFloat[0][1] = 1 / 4;
+	rightUpXFloat[0][2] = 3 / 4;
+	rightUpXFloat[0][3] = 7 / 4;
+	rightUpXFloat[1][0] = 5 / 4;
+	rightUpXFloat[1][1] = 1 / 4;
+	rightUpXFloat[1][2] = 3 / 4;
+	rightUpXFloat[1][3] = 7 / 4;
+	rightUpXFloat[2][0] = 5 / 4;
+	rightUpXFloat[2][1] = 1 / 4;
+	rightUpXFloat[2][2] = 3 / 4;
+	rightUpXFloat[2][3] = 7 / 4;
+	rightUpXFloat[3][0] = 5 / 4;
+	rightUpXFloat[3][1] = 1 / 4;
+	rightUpXFloat[3][2] = 3 / 4;
+	rightUpXFloat[3][3] = 7 / 4;
+
+	rightUpYFloat[0][0] = 7 / 4;
+	rightUpYFloat[0][1] = 7 / 4;
+	rightUpYFloat[0][2] = 7 / 4;
+	rightUpYFloat[0][3] = 7 / 4;
+	rightUpYFloat[1][0] = 3 / 4;
+	rightUpYFloat[1][1] = 3 / 4;
+	rightUpYFloat[1][2] = 3 / 4;
+	rightUpYFloat[1][3] = 3 / 4;
+	rightUpYFloat[2][0] = 1 / 4;
+	rightUpYFloat[2][1] = 1 / 4;
+	rightUpYFloat[2][2] = 1 / 4;
+	rightUpYFloat[2][3] = 1 / 4;
+	rightUpYFloat[3][0] = 5 / 4;
+	rightUpYFloat[3][1] = 5 / 4;
+	rightUpYFloat[3][2] = 5 / 4;
+	rightUpYFloat[3][3] = 5 / 4;
+
+	float sumForNormal = 0;
 	for (int i = 0; i < MAT_SIZE; i++) {
 		for (int j = 0; j < MAT_SIZE; j++) {
-			long uFuncAns = uFunc(rightUpX.at<long>(i, j)) * (uFunc(rightUpY.at<long>(i, j)));
+			//float absD = sqrt(pow(rightUpXLong[i][j],2) + pow(rightUpYLong[i][j],2));
+			//cout << absD << endl;
+			//float uFuncAns = uFunc(absD);
+			float uFuncAns = uFunc(rightUpXFloat[i][j])
+					* uFunc(rightUpYFloat[i][j]);
 			sumForNormal += uFuncAns;
 			rightUp.at<Vec3f>(i, j)(0) = uFuncAns;
 			rightUp.at<Vec3f>(i, j)(1) = uFuncAns;
@@ -185,75 +89,85 @@ void calcRightUp() {
 	}
 	for (int i = 0; i < MAT_SIZE; i++) {
 		for (int j = 0; j < MAT_SIZE; j++) {
-			rightUp.at<Vec3f>(i, j)(0) = rightUp.at<Vec3f>(i, j)(0) / sumForNormal;
-			rightUp.at<Vec3f>(i, j)(1) = rightUp.at<Vec3f>(i, j)(1) / sumForNormal;
-			rightUp.at<Vec3f>(i, j)(2) = rightUp.at<Vec3f>(i, j)(2) / sumForNormal;
+			rightUp.at<Vec3f>(i, j)(0) = rightUp.at<Vec3f>(i, j)(0)
+					/ sumForNormal;
+			rightUp.at<Vec3f>(i, j)(1) = rightUp.at<Vec3f>(i, j)(1)
+					/ sumForNormal;
+			rightUp.at<Vec3f>(i, j)(2) = rightUp.at<Vec3f>(i, j)(2)
+					/ sumForNormal;
 		}
 	}
+
+	delete rightUpXFloat;
+	delete rightUpYFloat;
 }
-void calcRightDown() {
-	Mat rightDownX = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	Mat rightDownY = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	rightDownX.at<long>(0, 0) = 5 / 4;
-	rightDownY.at<long>(0, 0) = 5 / 4;
-	rightDownX.at<long>(0, 1) = 1 / 4;
-	rightDownY.at<long>(0, 1) = 5 / 4;
-	rightDownX.at<long>(0, 2) = 3 / 4;
-	rightDownY.at<long>(0, 2) = 5 / 4;
-	rightDownX.at<long>(0, 3) = 7 / 4;
-	rightDownY.at<long>(0, 3) = 5 / 4;
-	rightDownX.at<long>(1, 0) = 5 / 4;
-	rightDownY.at<long>(1, 0) = 1 / 4;
-	rightDownX.at<long>(1, 1) = 1 / 4;
-	rightDownY.at<long>(1, 1) = 1 / 4;
-	rightDownX.at<long>(1, 2) = 3 / 4;
-	rightDownY.at<long>(1, 2) = 1 / 4;
-	rightDownX.at<long>(1, 3) = 7 / 4;
-	rightDownY.at<long>(1, 3) = 1 / 4;
-	rightDownX.at<long>(2, 0) = 5 / 4;
-	rightDownY.at<long>(2, 0) = 3 / 4;
-	rightDownX.at<long>(2, 1) = 1 / 4;
-	rightDownY.at<long>(2, 1) = 3 / 4;
-	rightDownX.at<long>(2, 2) = 3 / 4;
-	rightDownY.at<long>(2, 2) = 3 / 4;
-	rightDownX.at<long>(2, 3) = 7 / 4;
-	rightDownY.at<long>(2, 3) = 3 / 4;
-	rightDownX.at<long>(3, 0) = 5 / 4;
-	rightDownY.at<long>(3, 0) = 7 / 4;
-	rightDownX.at<long>(3, 1) = 1 / 4;
-	rightDownY.at<long>(3, 1) = 7 / 4;
-	rightDownX.at<long>(3, 2) = 3 / 4;
-	rightDownY.at<long>(3, 2) = 7 / 4;
-	rightDownX.at<long>(3, 3) = 7 / 4;
-	rightDownY.at<long>(3, 3) = 7 / 4;
-	long sumForNormal = 0;
+void calcRightDown(Mat src) {
+	float** rightDownXFloat = new float*[MAT_SIZE];
+	for (int i = 0; i < MAT_SIZE; ++i)
+		rightDownXFloat[i] = new float[MAT_SIZE];
+	float** rightDownYFloat = new float*[MAT_SIZE];
+	for (int i = 0; i < MAT_SIZE; ++i)
+		rightDownYFloat[i] = new float[MAT_SIZE];
+	rightDownXFloat[0][0] = 5 / 4;
+	rightDownXFloat[0][1] = 1 / 4;
+	rightDownXFloat[0][2] = 3 / 4;
+	rightDownXFloat[0][3] = 7 / 4;
+	rightDownXFloat[1][0] = 5 / 4;
+	rightDownXFloat[1][1] = 1 / 4;
+	rightDownXFloat[1][2] = 3 / 4;
+	rightDownXFloat[1][3] = 7 / 4;
+	rightDownXFloat[2][0] = 5 / 4;
+	rightDownXFloat[2][1] = 1 / 4;
+	rightDownXFloat[2][2] = 3 / 4;
+	rightDownXFloat[2][3] = 7 / 4;
+	rightDownXFloat[3][0] = 5 / 4;
+	rightDownXFloat[3][1] = 1 / 4;
+	rightDownXFloat[3][2] = 3 / 4;
+	rightDownXFloat[3][3] = 7 / 4;
+
+	rightDownYFloat[0][0] = 5 / 4;
+	rightDownYFloat[0][1] = 5 / 4;
+	rightDownYFloat[0][2] = 5 / 4;
+	rightDownYFloat[0][3] = 5 / 4;
+	rightDownYFloat[1][0] = 1 / 4;
+	rightDownYFloat[1][1] = 1 / 4;
+	rightDownYFloat[1][2] = 1 / 4;
+	rightDownYFloat[1][3] = 1 / 4;
+	rightDownYFloat[2][0] = 3 / 4;
+	rightDownYFloat[2][1] = 3 / 4;
+	rightDownYFloat[2][2] = 3 / 4;
+	rightDownYFloat[2][3] = 3 / 4;
+	rightDownYFloat[3][0] = 7 / 4;
+	rightDownYFloat[3][1] = 7 / 4;
+	rightDownYFloat[3][2] = 7 / 4;
+	rightDownYFloat[3][3] = 7 / 4;
+
+	float sumForNormal = 0;
 	for (int i = 0; i < MAT_SIZE; i++) {
 		for (int j = 0; j < MAT_SIZE; j++) {
-			long uFuncAns = uFunc(rightDownX.at<long>(i, j)) * (uFunc(rightDownY.at<long>(i, j)));
+			float uFuncAns = uFunc(rightDownXFloat[i][j]) * uFunc(rightDownYFloat[i][j]);
 			sumForNormal += uFuncAns;
 			rightDown.at<Vec3f>(i, j)(0) = uFuncAns;
 			rightDown.at<Vec3f>(i, j)(1) = uFuncAns;
 			rightDown.at<Vec3f>(i, j)(2) = uFuncAns;
-
 		}
 	}
 	for (int i = 0; i < MAT_SIZE; i++) {
 		for (int j = 0; j < MAT_SIZE; j++) {
-			rightDown.at<Vec3f>(i, j)(0) = rightDown.at<Vec3f>(i, j)(0) / sumForNormal;
-			rightDown.at<Vec3f>(i, j)(1) = rightDown.at<Vec3f>(i, j)(1) / sumForNormal;
-			rightDown.at<Vec3f>(i, j)(2) = rightDown.at<Vec3f>(i, j)(2) / sumForNormal;
+			rightDown.at<Vec3f>(i, j)(0) = rightDown.at<Vec3f>(i, j)(0)
+					/ sumForNormal;
+			rightDown.at<Vec3f>(i, j)(1) = rightDown.at<Vec3f>(i, j)(1)
+					/ sumForNormal;
+			rightDown.at<Vec3f>(i, j)(2) = rightDown.at<Vec3f>(i, j)(2)
+					/ sumForNormal;
 		}
 	}
 }
-void calcMatrices() {
-	leftUp = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	leftDown = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
+void calcMatrices(Mat src) {
 	rightUp = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
 	rightDown = Mat::zeros(MAT_SIZE, MAT_SIZE, src.type());
-	/*calcLeftUp();
-	calcLeftDown();*/
-	calcRightUp();
-	calcRightDown();
+	calcRightUp(src);
+	calcRightDown(src);
 }
 int main(int argc, char** argv) {
 	int wave_frequency = 3; //Default wave frequency
@@ -267,7 +181,7 @@ int main(int argc, char** argv) {
 	}
 	wave_frequency = atoi(argv[2]);
 	char* interpolation = argv[3];
-	src = imread(argv[1], IMREAD_COLOR);   // Read the file
+	Mat src = imread(argv[1], IMREAD_COLOR);   // Read the file
 	amplitude = amplitude * src.rows;   //% of picture height.
 
 	if ((!src.data) || src.empty())                   // Check for invalid input
@@ -287,8 +201,8 @@ int main(int argc, char** argv) {
 	//Set Background to white.
 	dst.setTo(Scalar(255, 255, 255));
 	dst_inter.setTo(Scalar(255, 255, 255));
-	cv::copyMakeBorder(src, srcWithMirror, 10, 10, 10, 10, cv::BORDER_REFLECT, 0);
-	calcMatrices();
+	cv::copyMakeBorder(src, srcWithMirror, 2, 2, 2, 2, cv::BORDER_REFLECT,0);
+	calcMatrices(src);
 	//Distort done here using forward scanning with no interpolation.
 	for (int y = 0; y < src.rows; y++) {
 		for (int x = 0; x < src.cols; x++) {
@@ -304,43 +218,45 @@ int main(int argc, char** argv) {
 
 	for (int dst_y = 0; dst_y < dst_inter.rows; dst_y++) {
 		for (int x = 0; x < dst_inter.cols; x++) {
-			double y = dst_y - amplitude * (1 + sin(wave_frequency * MY_PI * x / src.cols));
+			double y = dst_y - amplitude
+							* (1 + sin(wave_frequency * MY_PI * x / src.cols));
 			int src_y;
 			double src_y1, src_y2;
 			switch (interpolation[0]) {
 			case 'n':
-				src_y = ((int) round(y));
-				if ((src_y >= 0) && (src_y < src.rows))
-					dst_inter.at<Vec3b>(dst_y, x) = src.at<Vec3b>(src_y, x);
+				src_y = (int) round(y);
+				if (in_Range(src_y, 0, src.rows))
+					dst_inter.at<Vec3f>(dst_y, x) = src.at<Vec3f>(src_y, x);
 				break;
-			case 'b':                              //Bi-Linear
+			case 'b': //Bi-Linear
 				src_y1 = floor(y);
 				src_y2 = ceil(y);
 				if (in_Range(src_y1, 0, src.rows)) {
 					if (in_Range(src_y2, 0, src.rows)) {         //Both in range
-						dst_inter.at<Vec3b>(dst_y, x) = src.at<Vec3b>(
-								(int) src_y1, x) * (src_y2 - y)
-								+ src.at<Vec3b>((int) src_y2, x) * (y - src_y1);
+						for (int i = 0; i < 3; i++) {
+							dst_inter.at<Vec3b>(dst_y, x)(i) = src.at<Vec3b>(
+									(int) src_y1, x)(i) * (src_y2 - y)
+									+ src.at<Vec3b>((int) src_y2, x)(i)
+											* (y - src_y1);
+						}
 					} else {
 						// 1 in range
-						dst_inter.at<Vec3b>(dst_y, x) = src.at<Vec3b>(
+						dst_inter.at<Vec3f>(dst_y, x) = src.at<Vec3f>(
 								(int) src_y1, x);
 					}
 				} else if (in_Range(src_y2, 0, src.rows)) {
 					// 2 in range
-					dst_inter.at<Vec3b>(dst_y, x) = src.at<Vec3b>((int) src_y2,
-							x);
+					dst_inter.at<Vec3f>(dst_y, x) = src.at<Vec3f>((int) src_y2,x);
 				}                              //else None in range
 				break;
 			case 'c': {
 				//Cubic
-				float distFromSrcY = abs(y - dst_y);
-				Mat cropped;
+				float distFromSrcY = abs(y - src_y);
 				int mirrX = x + 2;
-				int mirrY = y + 2;
+				int mirrY = ((int)round(y)) + 2;
 				Mat toUse;
 				Point p1;
-				if (in_Range(y, 0, src.rows)) {
+				if (in_Range(round(y), 0, src.rows)) {
 					if (distFromSrcY > 0.5) { //Use topRight
 						toUse = rightDown;
 						p1 = Point(mirrX - 2, mirrY - 2);
@@ -349,13 +265,14 @@ int main(int argc, char** argv) {
 						p1 = Point(mirrX - 1, mirrY - 1);
 					}
 					Rect rect(p1, Size(4, 4));
-					cropped = srcWithMirror(rect);
-					Vec3f newVec = Vec3f(0,0,0);
-					for(int cubic_i = 0; cubic_i < MAT_SIZE; cubic_i++){
-						for(int cubic_j = 0; cubic_j < MAT_SIZE; cubic_j++){
-							newVec(0) += cropped.at<Vec3f>(cubic_i,cubic_j)(0) * toUse.at<Vec3f>(cubic_i,cubic_j)(0);
-							newVec(1) += cropped.at<Vec3f>(cubic_i,cubic_j)(1) * toUse.at<Vec3f>(cubic_i,cubic_j)(1);
-							newVec(3) += cropped.at<Vec3f>(cubic_i,cubic_j)(2) * toUse.at<Vec3f>(cubic_i,cubic_j)(2);
+					Mat cropped = srcWithMirror(rect);
+					Vec3f newVec = Vec3f(0, 0, 0);
+					for (int cubic_i = 0; cubic_i < MAT_SIZE; cubic_i++) {
+						for (int cubic_j = 0; cubic_j < MAT_SIZE; cubic_j++) {
+							for(int cubic_k = 0; cubic_k < 3; cubic_k++){
+								newVec(cubic_k) += cropped.at<Vec3f>(cubic_i, cubic_j)(cubic_k)
+												 * toUse.at<Vec3f>(cubic_i, cubic_j)(cubic_k);
+							}
 						}
 					}
 					dst_inter.at<Vec3f>(dst_y, x) = newVec;
@@ -382,6 +299,11 @@ int main(int argc, char** argv) {
 	imshow("With Interpolation", dst_inter);   // Create a window for display.
 
 	waitKey(0);                            // Wait for a keystroke in the window
+	/*src.release();
+	dst_inter.release();
+	dst.release();
+	rightDown.release();
+	rightUp.release();*/
 	return 0;
 }
 
